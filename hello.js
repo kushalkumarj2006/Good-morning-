5507fa81a1d8507c5116ec3f5c78d4ceb5460fbb
Hemanth898 <183099924+Hemanth898@users.noreply.github.com>
2025-07-29 22:36:25 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button');
+const spans = document.getElementById('updateContent');
+btn.onclick = function () {
+    const name = prompt('enter your name');
+    spans.innerHTML = `Good Night ${name}`;
+}
