74ea66090adf038abdb46d11adf3788cf8776527
Hemanth898 <183099924+Hemanth898@users.noreply.github.com>
2025-07-29 23:01:08 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ const btn = document.querySelector('button');
 const spans = document.getElementById('updateContent');
 btn.onclick = function () {
     const name = prompt('enter your name');
-    spans.innerHTML = `tuck you ${name}`;
+    spans.innerHTML = `Good Morning ${name}`;
 }
